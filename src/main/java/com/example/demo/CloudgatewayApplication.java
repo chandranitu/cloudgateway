37a1d7f7941404f54b8b39e119b80f748c58ea1c
chandrashekhar <chandranitu@gmail.com>
2024-07-18 10:20:18 +0530

    spring cloud

---
+++ src/main/java/com/example/demo/CloudgatewayApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+
+public class CloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routerBuilder(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route("userservice", r -> r.path("/userservice/**").uri("http://localhost:8081/"))
+				.route("backoffice", r -> r.path("/backoffice/**").uri("http://localhost:8082/")).build();
+	}
+
+}
