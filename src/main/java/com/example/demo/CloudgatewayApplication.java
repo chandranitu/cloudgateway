7bb54666f82de08acbf37181c80c9dd406fdcf00
chandrashekhar <chandranitu@gmail.com>
2024-09-02 14:07:41 +0530

    cloud

--- src/main/java/com/example/demo/CloudgatewayApplication.java
+++ src/main/java/com/example/demo/CloudgatewayApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class CloudgatewayApplication {
 
 	public static void main(String[] args) {
@@ -20,7 +20,8 @@ public class CloudgatewayApplication {
 	public RouteLocator routerBuilder(RouteLocatorBuilder routeLocatorBuilder) {
 		return routeLocatorBuilder.routes()
 				.route("userservice", r -> r.path("/userservice/**").uri("http://localhost:8081/"))
-				.route("backoffice", r -> r.path("/backoffice/**").uri("http://localhost:8082/")).build();
+				.route("backoffice", r -> r.path("/backoffice/**").uri("http://localhost:8082/"))
+		        .route("switch", r -> r.path("/switch/**").uri("http://localhost:8083/")).build();
 	}
 
 }
